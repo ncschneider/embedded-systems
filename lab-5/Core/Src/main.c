2b8f81634468d0004c59a5b508c7dd0d5d935d62
nicholas.schneider <nicholas.schneider@utah.edu>
2024-03-13 09:21:48 -0600

    added I2C init function, read/write prototypes

--- lab-5/Core/Src/main.c
+++ lab-5/Core/Src/main.c
@@ -19,42 +19,17 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
 /* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
+#define GYRO_ADDR 0b1101011
 
 /* Private variables ---------------------------------------------------------*/
 
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
+void i2c_init(uint8_t addr, uint8_t nBytes, uint8_t rw);
+void i2c_write(uint8_t data);
+uint8_t i2c_read(void);
 
 /**
   * @brief  The application entry point.
@@ -67,33 +42,60 @@ int main(void)
 	
 	// initialize clocks
 	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
-	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+	RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
+	
+	// initialize digital outputs PB14 and PC0
+	GPIOB->MODER |= (1 << 28);
+	GPIOB->OTYPER &= ~(1 << 14);
+	GPIOC->MODER |= (1 << 0);
+	GPIOC->OTYPER &= ~(1 << 0);
 	
 	// initialize I2C1
-	// SDA: GPIOB 7 -- AF1
-	// SCL: GPIOB 6 -- AF1
-	GPIOB->MODER |= (1 << 13) | (1 << 15);
-	GPIOB->OTYPER |= (1 << 6) | (1 << 7);
-	GPIOB->AFR[0] |= (1 << 24) | (1 << 28);
+	// SDA: GPIOB 11 -- AF1
+	// SCL: GPIOB 13 -- AF5
+	GPIOB->MODER |= (1 << 23) | (1 << 27);
+	GPIOB->OTYPER |= (1 << 11) | (1 << 13);
+	GPIOB->AFR[1] |= (1 << 12) | (1 << 20) | (1 << 22);
 	
-	/* Configure I2C1 timing
+	/* Configure I2C2 timing
 	 * Prescaler = 1
 	 * SCLDEL = 0x4
 	 * SDADEL = 0x2
 	 * SCLH = 0x0F
 	 * SCLL = 0x13
 	 */
-	I2C1->TIMINGR |= 0x10420F13;
+	I2C2->TIMINGR |= 0x10420F13;
 	
-	// enable I2C1
-	I2C1->CR1 |= (1 << 0);
+	// enable I2C2
+	I2C2->CR1 |= (1 << 0);
 	
-
+	// set PC0 and PB14 high
+	GPIOB->ODR |= (1 << 14);
+	GPIOC->ODR |= (1 << 0);
+	
   while (1)
   {
 		
   }
 }
+
+void i2c_init(uint8_t addr, uint8_t nBytes, uint8_t rw) {
+	I2C2->CR2 &= ~(0x3FF << 0) & ~(0xFF << 16); // clear address and number of bytes
+	I2C2->CR2 |= (nBytes << 16) | (addr << 1); // set number of bytes in transaction and slave address (shifted 1 for 7-bit addresses)
+	if(rw == 0) { // set or clear read/write bit depending on desired operation (0 = write, 1 = read)
+		I2C2->CR2 &= ~(1 << 10);
+	}
+	else if(rw == 1) {
+		I2C2->CR2 |= (1 << 10);
+	}
+}
+
+void i2c_write(uint8_t data) {
+	I2C1->CR2 |= (1 << 13); // start
+}
+
+uint8_t i2c_read(void);
 
 /**
   * @brief System Clock Configuration
