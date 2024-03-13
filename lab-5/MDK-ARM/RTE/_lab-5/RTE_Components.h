81ac576cf630482620eb883e1971fffa3eb13228
nicholas.schneider <nicholas.schneider@utah.edu>
2024-03-13 08:02:26 -0600

    add project files -- main under Core/Src

---
+++ lab-5/MDK-ARM/RTE/_lab-5/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'lab-5' 
+ * Target:  'lab-5' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
