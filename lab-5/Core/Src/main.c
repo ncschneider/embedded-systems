ad5d928ba82016a9c8c349d07f107e8c628acfb3
nicholas.schneider <nicholas.schneider@utah.edu>
2024-03-19 13:45:13 -0600

    **CHECKOFF 2**
    added functionality to read gyro x/y rotation data and light corresponding LEDs according to direction

--- lab-5/Core/Src/main.c
+++ lab-5/Core/Src/main.c
@@ -20,17 +20,26 @@
 #include "main.h"
 
 /* Private define ------------------------------------------------------------*/
-#define GYRO_ADDR 0b1101001
+#define GYRO_ADDR 0x69
 #define WHO_AM_I 0x0F
+#define CTRL_REG1 0x20
+#define OUT_X_L 0x28
+#define OUT_X_H 0x29
+#define OUT_Y_L 0x2A
+#define OUT_Y_H 0x2B
+#define OUT_X 0xA8
+#define OUT_Y 0xAA
 
 /* Private variables ---------------------------------------------------------*/
-uint8_t NUM_BYTES = 1;
+uint8_t NUM_BYTES = 2;
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 void i2c_init(uint8_t addr, uint8_t nBytes);
 void i2c_write(uint8_t data);
 uint8_t i2c_read(uint8_t addr);
+int16_t i2c_readTwo(uint8_t addr);
+void i2c_writeTwo(uint8_t addr, uint8_t data);
 
 /**
   * @brief  The application entry point.
@@ -42,6 +51,9 @@ int main(void)
   SystemClock_Config();
 	
 	uint8_t slaveData = 0x00;
+	uint32_t counter = 0;
+	int16_t xData = 0;
+	int16_t yData = 0;
 	
 	// initialize clocks
 	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
@@ -82,7 +94,7 @@ int main(void)
 	GPIOC->MODER |= (1 << 0);
 	GPIOC->OTYPER &= ~(1 << 0);
 	
-	// initialize I2C1
+	// initialize I2C2
 	// SDA: GPIOB 11 -- AF1
 	// SCL: GPIOB 13 -- AF5
 	GPIOB->MODER |= (1 << 23) | (1 << 27);
@@ -105,37 +117,70 @@ int main(void)
 	GPIOB->ODR |= (1 << 14);
 	GPIOC->ODR |= (1 << 0);
 	
-  while (1)
+	// initialize gyro control register for x and y axis
+	i2c_init(GYRO_ADDR, NUM_BYTES);
+	i2c_writeTwo(CTRL_REG1, 0x0B); // write 1 to x-axis enable, y-axis enable, and normal mode
+  
+	/*
+	* RED: LED3 -- +Y
+	* ORANGE: LED4 -- -X
+	* GREEN: LED5 -- +X
+	* BLUE: LED6 -- -Y
+	*/
+	while (1)
   {
-		i2c_init(GYRO_ADDR, NUM_BYTES);
-		slaveData = i2c_read(WHO_AM_I);
-		if(slaveData == 0xD3) {
-			GPIOC->ODR |= (1 << 9); // turn on green LED if data matches 
-		}
-		while(1) {}
+			// collect gyro data
+			xData = i2c_readTwo(OUT_X);
+			yData = i2c_readTwo(OUT_Y);
+			
+			// compare gyro data to predefined thresholds to determine rotation directions
+			if(xData > 200) {
+				GPIOC->ODR |= (1 << 9);
+				GPIOC->ODR &= ~(1 << 8);
+			}
+			else if(xData < -200) {
+				GPIOC->ODR |= (1 << 8);
+				GPIOC->ODR &= ~(1 << 9);
+			}
+			else {
+				GPIOC->ODR &= ~(1 << 8) & ~(1 << 9);
+			}
+			
+			if(yData > 200) {
+				GPIOC->ODR |= (1 << 6);
+				GPIOC->ODR &= ~(1 << 7);
+			}
+			else if(yData < -200) {
+				GPIOC->ODR |= (1 << 7);
+				GPIOC->ODR &= ~(1 << 6);
+			}
+			else {
+				GPIOC->ODR &= ~(1 << 6) & ~(1 << 7);
+			}
+			HAL_Delay(100);
   }
 }
 
+
+// initialize I2C transaction
 void i2c_init(uint8_t addr, uint8_t nBytes) {
-	I2C2->CR2 &= ~(0x3FF << 0) & ~(0xFF << 16); // clear address and number of bytes
+	I2C2->CR2 &= ~(0xFF << 1) & ~(0xFF << 16); // clear address and number of bytes
 	I2C2->CR2 |= (nBytes << 16) | (addr << 1); // set number of bytes in transaction and slave address (shifted 1 for 7-bit addresses)
 }
+
 
 // write one byte to the slave device
 void i2c_write(uint8_t data) {
 	I2C2->CR2 &= ~(1 << 10); // R/W bit set to 0 (write)
 	I2C2->CR2 |= (1 << 13); // start
-	while(!((I2C2->ISR >> 1) & 1)) { // wait for TXIS to be set
-		GPIOC->ODR |= (1 << 8); // orange on
+	while((I2C2->ISR & (1 << 1)) == 0) { // wait for TXIS to be set
 		if(I2C2->ISR & (1 << 4)) { // check for NACK
-			GPIOC->ODR |= (1 << 6); // red on
-			while(1) {}
 		}
 	}
 	I2C2->TXDR = data;
-	GPIOC->ODR &= ~(1 << 6) & ~(1 << 8); // turn off LEDs
 	while(!(I2C2->ISR & (1 << 6))) {} // wait for transaction to complete
 }
+
 
 // read one byte from the slave device
 uint8_t i2c_read(uint8_t addr) {
@@ -144,17 +189,60 @@ uint8_t i2c_read(uint8_t addr) {
 	i2c_init(GYRO_ADDR, NUM_BYTES);
 	I2C2->CR2 |= (1 << 10); // set R/W bit to 1 (read)
 	I2C2->CR2 |= (1 << 13); // start
-	while(!((I2C2->ISR >> 2) & 1)) { // wait for RXNE to be set
-		GPIOC->ODR |= (1 << 7); // blue on
+	while(((I2C2->ISR >> 2) & 1) == 0) { // wait for RXNE to be set
 		if(I2C2->ISR & (1 << 4)) { // check for NACK
-			GPIOC->ODR |= (1 << 6); // red on
 		}
 	}
 	while(!(I2C2->ISR & (1 << 6))) {} // wait for transaction to complete
 	data = I2C2->RXDR; // collect byte
 	I2C2->CR2 |= (1 << 14); // stop
-	GPIOC->ODR &= ~(1 << 6) & ~(1 << 7); // turn off LEDs
 	return data;
+}
+
+
+// read two bytes of data, only for sequential reads without restart
+int16_t i2c_readTwo(uint8_t addr) {
+	uint16_t data1, data2;
+	int16_t fullData;
+	i2c_init(GYRO_ADDR, 1);
+	i2c_write(addr);
+	i2c_init(GYRO_ADDR, NUM_BYTES);
+	I2C2->CR2 |= (1 << 10); // set R/W bit to 1 (read)
+	I2C2->CR2 |= (1 << 13); // start
+	while((I2C2->ISR & (1 << 2)) == 0) { // wait for RXNE to be set
+		if(I2C2->ISR & (1 << 4)) { // check for NACK
+		}
+	}
+	data1 = I2C2->RXDR; // collect byte 1
+	while((I2C2->ISR & (1 << 2)) == 0) { // wait for RXNE to be set
+		if(I2C2->ISR & (1 << 4)) { // check for NACK
+		}
+	}
+	data2 = I2C2->RXDR; // collect byte 2
+	I2C2->CR2 |= (1 << 14); // stop
+	fullData = data1 | (data2 << 8); // concatenate both bytes into one string of 16 signed bits
+	return fullData;
+}
+
+
+/*
+ * write two bytes of data, usually the first is an address, the second is the data to write
+ */
+void i2c_writeTwo(uint8_t addr, uint8_t data) {
+	I2C2->CR2 &= ~(1 << 10); // R/W bit set to 0 (write)
+	I2C2->CR2 |= (1 << 13); // start
+	while(((I2C2->ISR >> 1) & 1) == 0) { // wait for TXIS to be set
+		if(I2C2->ISR & (1 << 4)) { // check for NACK
+		}
+	}
+	I2C2->TXDR = addr;
+	while(((I2C2->ISR >> 1) & 1) == 0) { // wait for TXIS to be set
+		if(I2C2->ISR & (1 << 4)) { // check for NACK
+		}
+	}
+	I2C2->TXDR = data;
+	while(!(I2C2->ISR & (1 << 6))) {} // wait for transaction to complete
+	I2C2->CR2 |= (1 << 14); // stop
 }
 
 /**
